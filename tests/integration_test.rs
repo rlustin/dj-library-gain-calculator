7f85c05f2f829ad97aeee60f3b7a1b1daf1d04f3
Paul Adenot <paul@paul.cx>
2020-03-22 14:17:44 +0100

    Add a round-trip integration test

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,4 +1,5 @@
 use std::process::Command;
+use std::fs::read_to_string;
 
 use assert_cmd::prelude::*;
 
@@ -12,6 +13,24 @@ fn it_fails_when_file_does_not_exist() -> Result<(), Box<dyn std::error::Error>>
         .assert()
         .failure()
         .stdout("No such file or directory (os error 2)\naborting…\n");
+
+    Ok(())
+}
+
+#[test]
+fn it_parses_a_1_element_collection() -> Result<(), Box<dyn std::error::Error>> {
+    let mut command = Command::cargo_bin("dj-library-gain-calculator")?;
+
+    let input_path = "tests/vectors/1_element_collection.nml";
+    command.arg(input_path);
+    let mut string = read_to_string(input_path)?;
+    // The serializer doesn't produce a \n at the end, remove it from the file before comparing
+    string.truncate(string.len() - 1);
+
+    command
+        .assert()
+        .success()
+        .stdout(string);
 
     Ok(())
 }
