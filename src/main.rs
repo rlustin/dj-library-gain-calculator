bf9e5b1e3f9b87a659c33223f6c024dd1899f461
Paul Adenot <paul@paul.cx>
2020-03-22 11:49:43 +0100

    Add an optional argument for output stream

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
-use crate::traktor::deserialize_collection;
+use crate::traktor::*;
+use std::fs::File;
+use std::io::BufWriter;
 use clap::{load_yaml, App};
+use std::any::Any;
+use std::io::Write;
 
 mod analysis;
 mod error;
@@ -8,6 +12,39 @@ mod traktor;
 fn main() {
     let yaml = load_yaml!("cli.yml");
     let matches = App::from(yaml).get_matches();
+
+    let maybe_output_file: Result<Box<dyn Write>, _> = match matches.value_of("output") {
+        Some(output_path) => {
+            match output_path {
+                "-" => {
+                    Ok(Box::new(BufWriter::new(std::io::stdout())))
+                }
+                _ => {
+                    match File::create(output_path) {
+                        Ok(f) => {
+                            Ok(Box::new(BufWriter::new(f)))
+                        }
+                        Err(e) => {
+                            Err(e)
+                        }
+                    }
+                }
+            }
+        }
+        None => {
+            Ok(Box::new(BufWriter::new(std::io::stdout())))
+        }
+    };
+
+    let mut output_file = match maybe_output_file {
+        Ok(output_file) => {
+            output_file
+        },
+        Err(e) => {
+            exit_with_error(&e.to_string());
+            return;
+        }
+    };
 
     match matches.value_of("input") {
         Some(input) => match deserialize_collection(input) {
